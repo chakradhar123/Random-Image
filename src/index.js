cb57734a8b739eb58d4811844e84fa10c20cf671
chakradhar123 <chakradharvasurama@gmail.com>
2020-04-17 22:50:47 +0530

    deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express=require('express'),
     app=express();
     app.use(express.static("./"))
-    app.use(express.static("./js"));
+   
 app.get('/',(req,res)=>{
     res.sendFile("Home.html",{ root: "./"})
 })
